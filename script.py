fd686348cc1faec1bdec34d06014faf63db8ddf0
Shen Sat <shensat@shens-mbp.home>
2022-05-21 18:59:36 +0100

    Add test for 2x2 rect

--- script.py
+++ script.py
@@ -1,45 +1,32 @@
 import cv2 as cv
 
-image = cv.imread('2x2-red-blue-green-yellow.png')
+def create_pixel_data(image):
+  result = {}
+  for y_index, row in enumerate(image):
+    y = y_index
+    for x_index, column in enumerate(row):
+      x = x_index
+      if x in result:
+        result[x][y] = [column[2],column[1],column[0]]
+      else:
+        result[x] = {}
+        result[x][y] = [column[2],column[1],column[0]]
 
+  return result
 
-# print(image[0,2])
+# Tests
 
-result = {}
-for y_index, row in enumerate(image):
-  y = y_index
-  for x_index, column in enumerate(row):
-    x = x_index
-    if x in result:
-      result[x][y] = [column[2],column[1],column[0]]
-    else:
-      result[x] = {}
-      result[x][y] = [column[2],column[1],column[0]]
-    # print(f'x:{x} y:{y}')
+import unittest
 
+class PngToP8TestCase(unittest.TestCase):
 
-
-
-
-for key in result:
-  print(f'{key} => {result[key]}')
-
-# print(result[0][0])
-# print(result[1][0])
-# print(result[0][1])
-# print(result[1][1])
-
-
-
-
-
-
-
-# ------
-
-# hash = {
-# 	0 => { 1 => [255,255,255] }
-# }
-
-# result[0][1]
+  def test_create_pixel_data(self):  
+    
+    image = cv.imread('2x2-red-blue-green-yellow.png')
+    result = create_pixel_data(image)
+    expected_result = {
+      0: { 0: [255, 0, 77], 1: [0, 228, 54] },
+      1: { 0: [41, 173, 255], 1: [255, 236, 39] }
+    }
+    self.assertEqual(result, expected_result)
 

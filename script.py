28a62ee85100ef9b551ea1eae60b07f70b0e15d6
Shen Sat <shensat@shens-mbp.home>
2022-05-21 19:18:18 +0100

    Refactor and add test for 1x1 rect

--- script.py
+++ script.py
@@ -1,32 +1,39 @@
 import cv2 as cv
 
 def create_pixel_data(image):
-  result = {}
-  for y_index, row in enumerate(image):
-    y = y_index
-    for x_index, column in enumerate(row):
-      x = x_index
-      if x in result:
-        result[x][y] = [column[2],column[1],column[0]]
+  pixel_data = {}
+  for row_index, row in enumerate(image):
+    for cell_index, cell in enumerate(row):
+      if cell_index in pixel_data:
+        pixel_data[cell_index][row_index] = [cell[2],cell[1],cell[0]]
       else:
-        result[x] = {}
-        result[x][y] = [column[2],column[1],column[0]]
+        pixel_data[cell_index] = {}
+        pixel_data[cell_index][row_index] = [cell[2],cell[1],cell[0]]
 
-  return result
+  return pixel_data
 
 # Tests
 
 import unittest
 
-class PngToP8TestCase(unittest.TestCase):
+class CreatePixelDataCase(unittest.TestCase):
 
-  def test_create_pixel_data(self):  
+  def test_2x2_red_blue_green_yellow(self):
     
     image = cv.imread('2x2-red-blue-green-yellow.png')
     result = create_pixel_data(image)
     expected_result = {
       0: { 0: [255, 0, 77], 1: [0, 228, 54] },
       1: { 0: [41, 173, 255], 1: [255, 236, 39] }
+    }
+    self.assertEqual(result, expected_result)
+
+  def test_1x1_red(self):
+    
+    image = cv.imread('1x1-red.png')
+    result = create_pixel_data(image)
+    expected_result = {
+      0: { 0: [255, 0, 77]}
     }
     self.assertEqual(result, expected_result)
 

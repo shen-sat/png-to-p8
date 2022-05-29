e17f939f01d0bf73ee592b1ff1a5772006fb561c
Shen Sat <shensat@shens-mbp.home>
2022-05-29 13:53:10 +0100

    Add validations to same_color_fill_pixels

--- same_color_fill_pixels.py
+++ same_color_fill_pixels.py
@@ -1,9 +1,18 @@
 class PixelNotFound(Exception):
-    pass
+  pass
 
+class OriginSameRowAsCorner(Exception):
+  pass
 
+class OriginSameColumnAsCorner(Exception):
+  pass
 
 def is_rect(origin_pixel, corner_pixel, pixel_data):
+  if origin_pixel[0] == corner_pixel[0]:
+    raise OriginSameRowAsCorner(f'origin: {origin_pixel}, corner: {corner_pixel}')
+  elif origin_pixel[1] == corner_pixel[1]:
+    raise OriginSameColumnAsCorner(f'origin: {origin_pixel}, corner: {corner_pixel}')
+
   min_y = origin_pixel[0]
   min_x = origin_pixel[1]
   color = origin_pixel[2]
@@ -17,7 +26,7 @@ def is_rect(origin_pixel, corner_pixel, pixel_data):
         print(f'checking pixel at y:{y}, x:{x}...')
         pixel = pixel_data[y][x]
       except KeyError:
-        raise PixelNotFound(f'y:{y} and x:{x}')
+        raise PixelNotFound(f'y:{y}, x:{x}')
 
       if pixel == origin_pixel:
         print('pixel is origin')
@@ -36,7 +45,7 @@ import unittest
 
 class SameColorFillPixels(unittest.TestCase):
 
-  def test_corner_is_present(self):
+  def test_rect_is_present(self):
     pixel_data = {
       0: { 0: [0,0,['XXX']], 1: [0,1,['XXX']] },
       1: { 0: [1,0,['XXX']], 1: [1,1,['XXX']] }
@@ -49,43 +58,75 @@ class SameColorFillPixels(unittest.TestCase):
 
     self.assertEqual(result, True)
 
-  # def test_corner_is_NOT_present(self):
-  #   pixel_data = {
-  #     0: { 0: [0,0,['XXX']], 1: [0,1,['•••']], 2: [0,2,['•••']] },
-  #     1: { 0: [1,0,['•••']], 1: [1,1,['•••']], 2: [1,2,['•••']] },
-  #     2: { 0: [2,0,['•••']], 1: [2,1,['•••']], 2: [2,2,['XXX']] }
-  #   }
+  def test_rect_is_NOT_present(self):
+    pixel_data = {
+      0: { 0: [0,0,['XXX']], 1: [0,1,['•••']] },
+      1: { 0: [1,0,['•••']], 1: [1,1,['XXX']] },
+    }
 
-  #   origin_pixel = pixel_data[0][0]
+    origin_pixel = pixel_data[0][0]
+    corner_pixel = pixel_data[1][1]
 
-  #   result = is_rect(origin_pixel, pixel_data)
+    result = is_rect(origin_pixel, corner_pixel, pixel_data)
 
-  #   self.assertEqual(result, None)
+    self.assertEqual(result, False)
 
-  # def test_only_one_pixel_present(self):
-  #   pixel_data = {
-  #     0: { 0: [0,0,['XXX']] }
-  #   }
+  def test_rect_is_present_within_other_pixels(self):
+    pixel_data = {
+      0: { 0: [0,0,['•••']], 1: [0,1,['•••']], 2: [0,2,['•••']], 3: [0,3,['•••']], 4: [0,4,['•••']] },
+      1: { 0: [1,0,['•••']], 1: [1,1,['XXX']], 2: [1,2,['XXX']], 3: [1,3,['XXX']], 4: [1,4,['•••']] },
+      2: { 0: [2,0,['•••']], 1: [2,1,['XXX']], 2: [2,2,['XXX']], 3: [2,3,['XXX']], 4: [2,4,['•••']] },
+      3: { 0: [3,0,['•••']], 1: [3,1,['XXX']], 2: [3,2,['XXX']], 3: [3,3,['XXX']], 4: [3,4,['•••']] },
+      4: { 0: [4,0,['•••']], 1: [4,1,['•••']], 2: [4,2,['•••']], 3: [4,3,['•••']], 4: [4,4,['•••']] }
+    }
 
-  #   origin_pixel = pixel_data[0][0]
+    origin_pixel = pixel_data[1][1]
+    corner_pixel = pixel_data[3][3]
 
-  #   result = is_rect(origin_pixel, pixel_data)
+    result = is_rect(origin_pixel, corner_pixel, pixel_data)
 
-  #   self.assertEqual(result, None)
+    self.assertEqual(result, True)
 
-  # def test_gap_is_present(self):
-  #   pixel_data = {
-  #     0: { 0: [0,0,['XXX']], 1: [0,1,['•••']], 2: [0,2,['•••']] },
-  #     1: { 0: [1,0,['•••']],                   2: [1,2,['•••']] },
-  #     2: { 0: [2,0,['•••']], 1: [2,1,['•••']], 2: [2,2,['XXX']] }
-  #   }
+  def test_pixel_data_has_gaps(self):
+    pixel_data = {
+      0: { 0: [0,0,['XXX']]                   },
+      1: { 0: [1,0,['XXX']], 1: [1,1,['XXX']] }
+    }
 
-  #   origin_pixel = pixel_data[0][0]
-  #   corner_pixel = pixel_data[2][2]
+    origin_pixel = pixel_data[0][0]
+    corner_pixel = pixel_data[1][1]
 
-  #   result = is_rect(origin_pixel, pixel_data)
+    with self.assertRaises(PixelNotFound) as context:
+      is_rect(origin_pixel, corner_pixel, pixel_data)
 
-  #   self.assertEqual(result, None)
+    self.assertTrue('y:0, x:1' in str(context.exception))
+
+  def test_origin_same_row_as_corner(self):
+    pixel_data = {
+      0: { 0: [0,0,['XXX']], 1: [0,1,['XXX']] }
+    }
+
+    origin_pixel = pixel_data[0][0]
+    corner_pixel = pixel_data[0][1]
+
+    with self.assertRaises(OriginSameRowAsCorner) as context:
+      is_rect(origin_pixel, corner_pixel, pixel_data)
+
+    self.assertTrue("origin: [0, 0, ['XXX']], corner: [0, 1, ['XXX']]" in str(context.exception))
+
+  def test_origin_same_column_as_corner(self):
+    pixel_data = {
+      0: { 0: [0,0,['XXX']] },
+      1: { 0: [1,0,['XXX']] },
+    }
+
+    origin_pixel = pixel_data[0][0]
+    corner_pixel = pixel_data[1][0]
+
+    with self.assertRaises(OriginSameColumnAsCorner) as context:
+      is_rect(origin_pixel, corner_pixel, pixel_data)
+
+    self.assertTrue("origin: [0, 0, ['XXX']], corner: [1, 0, ['XXX']]" in str(context.exception))
 
 if __name__ == '__main__':
     unittest.main()

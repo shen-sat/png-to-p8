6b3eccfbbf83d0db8423cb126d3af1c0bb3e54aa
Shen Sat <shensat@shens-mbp.home>
2022-06-01 19:23:46 +0100

    Refactor CreatePixelData to create pixel_data without coordinates inside each pixel

--- script.py
+++ script.py
@@ -5,10 +5,10 @@ def create_pixel_data(image):
   for y, row in enumerate(image):
     for x, column in enumerate(row):
       if y in pixel_data:
-        pixel_data[y][x] = [y,x,[column[2],column[1],column[0]]]
+        pixel_data[y][x] = [column[2],column[1],column[0]]
       else:
         pixel_data[y] = {}
-        pixel_data[y][x] = [y,x,[column[2],column[1],column[0]]]
+        pixel_data[y][x] = [column[2],column[1],column[0]]
 
   return pixel_data
 
@@ -23,7 +23,7 @@ class CreatePixelDataCase(unittest.TestCase):
     image = cv.imread('1x1-red.png')
     result = create_pixel_data(image)
     expected_result = {
-      0: { 0: [0,0,[255,0,77]] }
+      0: { 0:[255,0,77] }
     }
     self.assertEqual(result, expected_result)
 
@@ -32,8 +32,8 @@ class CreatePixelDataCase(unittest.TestCase):
     image = cv.imread('2x2-red.png')
     result = create_pixel_data(image)
     expected_result = {
-      0: { 0: [0,0,[255,0,77]], 1: [0,1,[255,0,77]] },
-      1: { 0: [1,0,[255,0,77]], 1: [1,1,[255,0,77]] }
+      0: { 0: [255,0,77], 1: [255,0,77] },
+      1: { 0: [255,0,77], 1: [255,0,77] }
     }
     self.assertEqual(result, expected_result)
 
@@ -42,8 +42,8 @@ class CreatePixelDataCase(unittest.TestCase):
     image = cv.imread('2x2-red-red-blue-blue.png')
     result = create_pixel_data(image)
     expected_result = {
-      0: { 0: [0,0,[255,0,77]],   1: [0,1,[255,0,77]] },
-      1: { 0: [1,0,[41,173,255]], 1: [1,1,[41,173,255]] }
+      0: { 0: [255,0,77],   1: [255,0,77] },
+      1: { 0: [41,173,255], 1: [41,173,255] }
     }
     self.assertEqual(result, expected_result)
 
@@ -52,8 +52,8 @@ class CreatePixelDataCase(unittest.TestCase):
     image = cv.imread('2x2-red-blue-red-blue.png')
     result = create_pixel_data(image)
     expected_result = {
-      0: { 0: [0,0,[255,0,77]], 1: [0,1,[41,173,255]] },
-      1: { 0: [1,0,[255,0,77]], 1: [1,1,[41,173,255]] }
+      0: { 0: [255,0,77], 1: [41,173,255] },
+      1: { 0: [255,0,77], 1: [41,173,255] }
     }
     self.assertEqual(result, expected_result)
 
@@ -62,8 +62,8 @@ class CreatePixelDataCase(unittest.TestCase):
     image = cv.imread('2x2-red-blue-green-yellow.png')
     result = create_pixel_data(image)
     expected_result = {
-      0: { 0: [0,0,[255,0,77]], 1: [0,1,[41,173,255]] },
-      1: { 0: [1,0,[0,228,54]], 1: [1,1,[255,236,39]] }
+      0: { 0: [255,0,77], 1: [41,173,255] },
+      1: { 0: [0,228,54], 1: [255,236,39] }
     }
     self.assertEqual(result, expected_result)
 
